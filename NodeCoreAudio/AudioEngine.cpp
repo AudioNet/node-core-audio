e0fadcf474e99909372592a2f1d777f226f3ccb5
ZECTBynmo <michael.vegeto@gmail.com>
2012-08-17 22:20:49 -0400

    Added a lot of v8 related code to AudioEngine

--- NodeCoreAudio/AudioEngine.cpp
+++ NodeCoreAudio/AudioEngine.cpp
@@ -7,12 +7,16 @@
 #include "AudioEngine.h"
 #include "portaudio.h"
 #include <v8.h>
+#include <node_internals.h>
+#include <node_object_wrap.h>
 using namespace v8;
+
+Persistent<Function> Audio::AudioEngine::constructor;
 
 //////////////////////////////////////////////////////////////////////////////
 /*! Initalize */
-Audio::AudioEngine::AudioEngine( Persistent<Function>& audioCallback ) :
-	m_audioCallback(audioCallback),
+Audio::AudioEngine::AudioEngine( Local<Function>& audioCallback ) :
+	m_audioCallback(Persistent<Function>::New(audioCallback)),
 	m_uNumInputChannels(2),
 	m_uNumOutputChannels(2) {
 
@@ -28,14 +32,93 @@ Audio::AudioEngine::AudioEngine( Persistent<Function>& audioCallback ) :
 								2,          /* stereo output */
 								paFloat32,  /* 32 bit floating point output */
 								SAMPLE_RATE,
-								256,        /* frames per buffer, i.e. the number
-													of sample frames that PortAudio will
-													request from the callback. Many apps
-													may want to use
-													paFramesPerBufferUnspecified, which
-													tells PortAudio to pick the best,
-													possibly changing, buffer size.*/
+								256,        /* frames per buffer */
 								audioCallbackSource, /* this is your callback function */
-								&data ); /*This is a pointer that will be passed to
-													your callback*/
+								this ); /*This is a pointer that will be passed to your callback*/
+
 } // end AudioEngine::AudioEngine()
+
+
+//////////////////////////////////////////////////////////////////////////////
+/*! Our main audio callback */
+int Audio::AudioEngine::audioCallback( const void *input, void *output, unsigned long uSampleFrames, const PaStreamCallbackTimeInfo* timeInfo, PaStreamCallbackFlags statusFlags ) {
+	std::vector<float> teste;
+
+	m_uSampleFrames= Number::New( uSampleFrames );
+	//m_inputBuffer= Number::New( teste );
+	//m_outputBuffer= Array::New( outputBuffer );
+	float* outputSamples= reinterpret_cast<float *>( output );
+
+	// Copy output 
+	m_outputBuffer = v8::Array::New( uSampleFrames );
+	for (int iSample = 0; iSample < (signed)uSampleFrames; iSample++) {
+		float fSample= outputSamples[iSample];
+		m_outputBuffer->Set(v8::Number::New(iSample), v8::Number::New(fSample));
+	}
+
+	//context->Global()->Set(v8::String::New("arguments"), outputBuffer);
+
+	vector<float> pfloats;
+
+	ObjectWrap::Wrap( m_outputBuffer );
+
+	const unsigned argc = 3;
+	Local<Value> argv[argc] = { Local<Value>::New(m_uSampleFrames), m_inputBuffer, m_outputBuffer };
+	//cb->Call(Context::GetCurrent()->Global(), argc, argv);
+
+	m_audioCallback->Call( Context::GetCurrent()->Global(), argc, argv );
+
+	return 0;
+} // end AudioEngine::audioCallback()
+
+
+//////////////////////////////////////////////////////////////////////////////
+/*! Our node.js instantiation function */
+void Audio::AudioEngine::Init(v8::Handle<v8::Object> target) {
+	// Prepare constructor template
+	Local<FunctionTemplate> tpl = FunctionTemplate::New(New);
+	tpl->SetClassName(String::NewSymbol("AudioEngine"));
+	tpl->InstanceTemplate()->SetInternalFieldCount(1);
+	// Prototype
+// 	tpl->PrototypeTemplate()->Set(String::NewSymbol("plusOne"),
+// 		FunctionTemplate::New(PlusOne)->GetFunction());
+
+	constructor = Persistent<Function>::New(tpl->GetFunction());
+} // end AudioEngine::Init()
+
+
+//////////////////////////////////////////////////////////////////////////////
+/*! Instantiate from instance factory */
+v8::Handle<v8::Value> Audio::AudioEngine::NewInstance(const v8::Arguments& args) {
+	HandleScope scope;
+
+	const unsigned argc = 1;
+	Handle<Value> argv[argc] = { args[0] };
+	Local<Object> instance = constructor->NewInstance(argc, argv);
+
+	return scope.Close(instance);
+} // end AudioEngine::NewInstance()
+
+
+//////////////////////////////////////////////////////////////////////////////
+/*! Our V8 new operator */
+v8::Handle<v8::Value> Audio::AudioEngine::New(const v8::Arguments& args) {
+	HandleScope scope;
+
+	if (!args[0]->IsFunction()) {
+		return ThrowException( Exception::TypeError(String::New("Callback function required")) );
+	}
+	
+	Local<Function> callback = Local<Function>::Cast(args[0]);
+	
+	AudioEngine* engine = new AudioEngine( callback );
+
+	return args.This();
+} // end AudioEngine::New()
+
+
+//////////////////////////////////////////////////////////////////////////////
+/*! Destructor */
+Audio::AudioEngine::~AudioEngine() {
+	
+} // end AudioEngine::~AudioEngine()
--- NodeCoreAudio/AudioEngine.h
+++ NodeCoreAudio/AudioEngine.h
@@ -8,97 +8,47 @@
 #include "portaudio.h"
 #include <v8.h>
 #include <vector>
+#include <node_internals.h>
+#include <node_object_wrap.h>
 using namespace v8; using namespace std;
 
 #define SAMPLE_RATE (44100)
 
 namespace Audio {
 
-
-	typedef struct {
-		float left_phase;
-		float right_phase;
-	} paTestData;
-
-	static paTestData data;
-
-	/* This routine will be called by the PortAudio engine when audio is needed.
-	 It may called at interrupt level on some machines so don't do anything
-	 that could mess up the system like calling malloc() or free().
-	*/ 
-	static int patestCallback( const void *inputBuffer, void *outputBuffer,
-							   unsigned long framesPerBuffer,
-							   const PaStreamCallbackTimeInfo* timeInfo,
-							   PaStreamCallbackFlags statusFlags,
-							   void *userData )
-	{
-		
-
-		/* Cast data passed through stream to our structure. */
-// 		paTestData *data = (paTestData*)userData; 
-// 		float *out = (float*)outputBuffer;
-// 		unsigned int i;
-// 		(void) inputBuffer; /* Prevent unused variable warning. */
-//     
-// 		for( i=0; i<framesPerBuffer; i++ )
-// 		{
-// 			*out++ = data->left_phase;  /* left */
-// 			*out++ = data->right_phase;  /* right */
-// 			/* Generate simple sawtooth phaser that ranges between -1.0 and 1.0. */
-// 			data->left_phase += 0.01f;
-// 			/* When signal reaches top, drop back down. */
-// 			if( data->left_phase >= 1.0f ) data->left_phase -= 2.0f;
-// 			/* higher pitch so we can distinguish left and right. */
-// 			data->right_phase += 0.03f;
-// 			if( data->right_phase >= 1.0f ) data->right_phase -= 2.0f;
-// 		}
-		return 0;
-	}
-
 	//////////////////////////////////////////////////////////////////////////
 	//! Core audio functionality
-	class AudioEngine {
+	class AudioEngine : public node::ObjectWrap {
 	public:
-		AudioEngine( Persistent<Function>& audioCallback ); //!< Initalize
-		
-		int audioCallbackSource( const void *inputBuffer, void *outputBuffer, unsigned long framesPerBuffer, 
-								 const PaStreamCallbackTimeInfo* timeInfo, PaStreamCallbackFlags statusFlags, void *userData ) {
-			
-// 			paTestData *data = (paTestData*)userData; 
-// 			float *out = (float*)outputBuffer;
-// 			unsigned int i;
-// 			(void) inputBuffer; /* Prevent unused variable warning. */
-// 
-// 			for( i=0; i<framesPerBuffer; i++ )
-// 			{
-// 				*out++ = data->left_phase;  /* left */
-// 				*out++ = data->right_phase;  /* right */
-// 				/* Generate simple sawtooth phaser that ranges between -1.0 and 1.0. */
-// 				data->left_phase += 0.01f;
-// 				/* When signal reaches top, drop back down. */
-// 				if( data->left_phase >= 1.0f ) data->left_phase -= 2.0f;
-// 				/* higher pitch so we can distinguish left and right. */
-// 				data->right_phase += 0.03f;
-// 				if( data->right_phase >= 1.0f ) data->right_phase -= 2.0f;
-// 			}
-			std::vector<float> teste;
-	
-			m_uSampleFrames= Number::New( framesPerBuffer );
-			m_inputBuffer= Number::New( teste );
-			//m_outputBuffer= Array::New( outputBuffer );
+		AudioEngine( Local<Function>& audioCallback ); //!< Initalize
 
-			m_audioCallback->Call( m_uSampleFrames, m_inputBuffer, m_outputBuffer );
-			
-			return 0;
-		}
+		static void Init(v8::Handle<v8::Object> target);	//!< Our node.js instantiation function
+		static v8::Handle<v8::Value> NewInstance(const v8::Arguments& args);	//!< Instantiate from instance factory
+		
+		//! Our static audio callback function. This is static, and we pass a pointer to ourselves in as userData,
+		//! allowing us to call a non-static function on the correct instance of AudioEngine.
+		static int audioCallbackSource( const void *inputBuffer, void *outputBuffer, unsigned long framesPerBuffer, 
+								 const PaStreamCallbackTimeInfo* timeInfo, PaStreamCallbackFlags statusFlags, void *userData ) {
+			return ((AudioEngine*)userData)->audioCallback(inputBuffer, outputBuffer, framesPerBuffer, timeInfo, statusFlags);
+		} // end audioCallbackSource()
+
+
+		//!< Our main audio callback
+		int audioCallback( const void *input, void *output, unsigned long uSampleFrames, const PaStreamCallbackTimeInfo* timeInfo, PaStreamCallbackFlags statusFlags );
+
+		~AudioEngine();	//!< Destructor
+
 	private:
+		static v8::Persistent<v8::Function> constructor;				
+		static v8::Handle<v8::Value> New(const v8::Arguments& args);	//!< Our V8 new operator
+
 		int m_uNumInputChannels,
 			m_uNumOutputChannels;
 		
 		Persistent<Function> m_audioCallback;		//!< We call this with audio data whenever we get it
 		Handle<Value> m_uSampleFrames;				//!< Number of sample frames in this buffer (set in audioCallbackSource)
-		Handle<Value> m_inputBuffer,
-					  m_outputBuffer;
+		v8::Local<v8::Array> m_inputBuffer,
+							 m_outputBuffer;
 
 	}; // end class AudioEngine
 	
