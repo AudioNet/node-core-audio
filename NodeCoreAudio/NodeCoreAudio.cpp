3ffc02ea8e5427388848bd7740985fc94df86167
ZECTBynmo <michael.vegeto@gmail.com>
2012-08-17 22:18:44 -0400

    start to setup NodeCoreAudio as an AudioEngine factory

--- NodeCoreAudio/NodeCoreAudio.cpp
+++ NodeCoreAudio/NodeCoreAudio.cpp
@@ -1,6 +1,6 @@
 // NodeCoreAudio.cpp : Defines the exported functions for the DLL application.
 //
-
+#define BUILDING_NODE_EXTENSION
 #include "stdafx.h"
 
 #include <v8.h>
@@ -8,14 +8,14 @@ using namespace v8;
 #pragma comment(lib, "node")
 #include <node.h>
 
+#include <string>
+#include "AudioEngine.h"
 
 #include <WinSock2.h>
 #include <IPHlpApi.h>
 #include <stdio.h>
 #include <Windows.h>
 #pragma comment(lib, "IPHLPAPI.lib")
-#include <string>
-#include "AudioEngine.h"
 
 
 #define MALLOC(x) HeapAlloc(GetProcessHeap(), 0, (x))
@@ -42,44 +42,60 @@ void CallCallback(uv_work_t* req) {
 } // end CallCallback
 
 
-namespace NodeCoreAudio {
+namespace CoreAudio {
 	Handle<Value> InitializeAudioEngine(const Arguments &args) {
+
 		HandleScope scope;
-		
-		if (!args[0]->IsFunction()) {
-			return ThrowException( Exception::TypeError(String::New("Callback function required")) );
-		}
+		return scope.Close(Audio::AudioEngine::NewInstance(args));
 
-		Local<Function> callback = Local<Function>::Cast(args[0]);
-
-		Baton* baton = new Baton();
-		baton->request.data = baton;
-		baton->callback = Persistent<Function>::New(callback);
-
-		//uv_queue_work(uv_default_loop(), &baton->request, AsyncWork, AsyncAfter);
-
-		Audio::AudioEngine* pAudioEngine= new Audio::AudioEngine( baton->callback );
-
-		return scope.Close(Undefined());
+// 		HandleScope scope;
+// 		
+// 		if (!args[0]->IsFunction()) {
+// 			return ThrowException( Exception::TypeError(String::New("Callback function required")) );
+// 		}
+// 
+// 		Local<Function> callback = Local<Function>::Cast(args[0]);
+// 
+// 		Baton* baton = new Baton();
+// 		baton->request.data = baton;
+// 		baton->callback = Persistent<Function>::New(callback);
+// 
+// 		//uv_queue_work(uv_default_loop(), &baton->request, AsyncWork, AsyncAfter);
+// 
+// 		Audio::AudioEngine* pAudioEngine= new Audio::AudioEngine( baton->callback );
+// 
+// 		return scope.Close(Undefined());
 
 	} // end InitializeAudioEngine()
 }
 
-void AsyncWork(uv_work_t* req) {
-	// No HandleScope!
+// void AsyncWork(uv_work_t* req) {
+// 	// No HandleScope!
+// 
+// 	Baton* baton = static_cast<Baton*>(req->data);
+// 
+// 	// Do work in threadpool here.
+// 	// Set baton->error_code/message on failures.
+// }
 
-	Baton* baton = static_cast<Baton*>(req->data);
+// extern "C" void NODE_EXTERN init (Handle<Object> target)
+// {
+// 	HandleScope scope;
+// 	Local<FunctionTemplate> initializeAudioEngine = FunctionTemplate::New( NodeCoreAudio::InitializeAudioEngine );
+// 	
+// 	target->Set( String::NewSymbol("initializeAudioEngine"), initializeAudioEngine->GetFunction() );
+// }
 
-	// Do work in threadpool here.
-	// Set baton->error_code/message on failures.
-}
-
-extern "C" void NODE_EXTERN init (Handle<Object> target)
-{
+Handle<Value> CreateEngine(const Arguments& args) {
 	HandleScope scope;
-	Local<FunctionTemplate> initializeAudioEngine = FunctionTemplate::New( NodeCoreAudio::InitializeAudioEngine );
-	
-	target->Set( String::NewSymbol("initializeAudioEngine"), initializeAudioEngine->GetFunction() );
+	return scope.Close(Audio::AudioEngine::NewInstance(args));
 }
 
-NODE_MODULE(NodeCoreAudio, init)
+void InitAll(Handle<Object> target) {
+	Audio::AudioEngine::Init( target );
+
+	target->Set(String::NewSymbol("createAudioEngine"),
+		FunctionTemplate::New(CreateEngine)->GetFunction());
+}
+
+NODE_MODULE( NodeCoreAudio, InitAll )
