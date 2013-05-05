566a36b974dce66c3cfec466a8d4443e1df52a74
ZECTBynmo <michael.vegeto@gmail.com>
2013-05-04 22:35:56 -0400

    update test

--- secondtest.js
+++ secondtest.js
@@ -1,11 +1,30 @@
 var engine = require("./node-core-audio").createNewAudioEngine();
 var outputBuffer = [];
 
+var options = {
+	inputChannels: 1,
+	framesPerBuffer: 512,
+}
+
+var hasdone = false;
+
+//engine.setOptions( options );
+
 engine.addAudioCallback( function(inputBuffer) { 
-	outputBuffer = inputBuffer[0];
+	outputBuffer = inputBuffer;
 
 	for( var iSample=0; iSample<outputBuffer[0].length; ++iSample ) {
 		outputBuffer[0][iSample] = iSample / outputBuffer[0].length;
+	}
+
+	//console.log( inputBuffer.length + " channels" );
+	//console.log( inputBuffer[0].length + " samples" );
+	//console.log( outputBuffer.length + " channels" );
+	//console.log( outputBuffer[0].length + " samples" );
+
+	if( !hasdone ) {
+		console.log( inputBuffer );
+		hasdone = true;
 	}
 
 	return outputBuffer;
